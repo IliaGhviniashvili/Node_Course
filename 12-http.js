8399a1bfb3271a5a6cf4ba48e13587e395e18b0b
Ilia Ghviniashvili <ili.ghviniashvili@gmail.com>
2023-11-11 21:51:40 +0400

    add faq page

--- 12-http.js
+++ 12-http.js
@@ -7,6 +7,9 @@ const server = http.createServer((req,res)=>{
     if(req.url === '/about'){
         res.end('Here is our short history')
     }
+    if(req.url === '/faq'){
+        res.end('here are frequently asked questions')
+    }
     res.end(`
     <h1>Oops!</h1>
     <p>We can't seem to find the page you are looking for</p>
